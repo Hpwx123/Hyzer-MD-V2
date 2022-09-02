3364ebd2433f22b79617731ffaef1c29b5cebeb6
Hpwx123 <112678279+Hpwx123@users.noreply.github.com>
2022-09-02 11:23:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6285822347348'] // Put your number here
+global.owner = ['6289528935082'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
